7e93280e57ab1ef4e35c0f64a988ad8e94d25611
Audrey-Sanchez <audrey1010@live.fr>
2025-01-13 16:44:45 +0100

    Fix drop down menu

--- ClickMe/app/src/main/java/com/progmobile/clickme/ui/levels/DropDownMenu.kt
+++ ClickMe/app/src/main/java/com/progmobile/clickme/ui/levels/DropDownMenu.kt
@@ -1,5 +1,7 @@
 package com.progmobile.clickme.ui.levels
 
+import android.content.res.Configuration
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -49,9 +52,6 @@ fun DropDownMenu(
 
     LazyColumn(
         modifier = modifier,
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        verticalArrangement = Arrangement.Center
-        //verticalArrangement = Arrangement.SpaceBetween
     ) {
         item {
             Text(
@@ -66,7 +66,7 @@ fun DropDownMenu(
 
         item {
             Text(
-                text = sentence,
+                text =  stringResource(id = R.string.dropdownmenu_indication) +" : $sentence",
                 style = MaterialTheme.typography.bodyLarge,
                 modifier = Modifier
                     .fillMaxWidth()
@@ -89,8 +89,6 @@ fun DropDownMenu(
                     DropdownMenu(
                         expanded = expanded,
                         onDismissRequest = { expanded = false },
-                        //modifier = modifier
-                        //offset = DpOffset(150.dp, 690.dp)
                     ) {
                         when (currentMenu) {
                             1 -> {
